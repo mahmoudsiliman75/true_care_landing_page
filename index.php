3c2bfdd265fbdade15ab8e3d8d543262c99dbe1e
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-04-21 14:56:03 +0200

    finish about section

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!-- END:: INCLUDING THE HEADER -->
   
   <!-- START:: HOME SECTION -->
-  <div id="intro_sec" class="home_sec">
+  <div id="intro_sec" class="intro_sec">
     <!-- START:: HOME SECTION CURVE -->
     <img src="assets/media/shp01.png">
     <!-- END:: HOME SECTION CURVE -->
@@ -45,27 +45,103 @@
       </div>
     </div>
     <!-- END:: HOME INFO SECTION -->
+  </div>
+  <!-- END:: HOME SECTION -->
 
-    <!-- START:: ABOUT SECTION -->
-    <div class="about_sec">
-      <div class="container_fluid">
-        <div class="col-12 col-md-6">
-          <div class="text_wraper">
-
+  <!-- START:: ABOUT SECTION -->
+  <div class="about_sec">
+    <div class="container-fluid">
+      <div class="row align-items-center">
+        <div class="col-12 col-lg-6 mb-4">
+          <div class="about_content_wraper">
+            <h2> 
+              What Heighlights 
+              <span>
+                <span>True</span> 
+                Care
+              </span> 
+            </h2>
+            <div class="text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ea placeat voluptas laboriosam soluta expedita aperiam, 
+              repellendus labore, enim porro, nihil aliquid. Culpa labore 
+              nulla cumque quam, quasi ab dolor repellendus! 
+            </div>
           </div>
         </div>
 
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-lg-6">
           <div class="cards_wraper">
+            <div class="row">
+              <div class="col-12 col-lg-6 mb-4">
+                <div class="card">
+                  <div class="icon_wraper">
+                    <img src="assets/media/icons/star.png">
+                  </div>
 
+                  <div class="card_details">
+                    <h4> Upgrade your work Performace </h4>
+                    <div>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Praesentium harum natus.
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-lg-6 mb-4">
+                <div class="card">
+                  <div class="icon_wraper">
+                    <img src="assets/media/icons/data.png">
+                  </div>
+
+                  <div class="card_details">
+                    <h4> Data Analyse </h4>
+                    <div>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Praesentium harum natus.
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-lg-6 mb-4">
+                <div class="card">
+                  <div class="icon_wraper">
+                    <img src="assets/media/icons/list.png">
+                  </div>
+
+                  <div class="card_details">
+                    <h4> Full Reports </h4>
+                    <div>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Praesentium harum natus.
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-lg-6 mb-4">
+                <div class="card">
+                  <div class="icon_wraper">
+                    <img src="assets/media/icons/watch.png">
+                  </div>
+
+                  <div class="card_details">
+                    <h4> Manage Your Scheduals </h4>
+                    <div>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Praesentium harum natus.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <!-- END:: ABOUT SECTION -->
-
   </div>
-  <!-- END:: HOME SECTION -->
+  <!-- END:: ABOUT SECTION -->
 
 <!-- START:: INCLUDING THE FOOTER -->
 <?php include('layouts/TheFooter.php')?>
