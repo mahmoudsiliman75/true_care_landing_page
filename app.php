b70f29ee447020b8d2fc161b93be486f6504974c
MahmoudSiliman <modysiliman75@gmail.com>
2021-04-27 00:53:19 +0200

    edit promo video section

--- app.php
+++ app.php
@@ -144,6 +144,14 @@
 
   <!-- START:: PROMO VIDEO SECTION -->
   <div id="promo_sec" class="promo_sec">
+    <!-- START:: UPPER CURVE -->
+    <div class="upper_curve">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+      </svg>
+    </div>
+    <!-- END:: UPPER CURVE -->
+
     <div class="video_wrape" style="background: url('https://i.ytimg.com/vi_webp/3chGVpR9NzA/maxresdefault.webp');">
       <div class="overlay">
         <!-- Button trigger modal -->
@@ -164,6 +172,14 @@
         <!-- END:: DEMO MODAL -->
       </div>
     </div>
+
+    <!-- START:: LOWER CURVE -->
+    <div class="lower_curve">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+      </svg>
+    </div>
+    <!-- END:: LOWER CURVE -->
   </div>
   <!-- END:: PROMO VIDEO SECTION -->
 
